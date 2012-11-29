4ec3bba4a5af11ff18af1cb513f4d3ae8e549483
Brian Flad <bflad417@gmail.com>
2012-11-29 14:10:45 -0500

    FC007 fix and additional information in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
+name              "shibboleth-idp"
 maintainer        "The Wharton School - The University of Pennsylvania"
 maintainer_email  "chef-admins@wharton.upenn.edu"
 license           "Apache 2.0"
 description       "Installs/Configures Shibboleth Identity Provider"
 version           "0.1.0"
 recipe            "shibboleth-idp", "Installs and enables Shibboleth IdP on Tomcat."
+recipe            "shibboleth-idp::iptables", "Sets up iptables with port forwarding."
+recipe            "shibboleth-idp::trustedcerts", "Installs Java trusted certificates."
 
 %w{ tomcat java }.each do |d|
   depends d
