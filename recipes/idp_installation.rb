c298dafefca6df64b668ec11b1f46326625c5608
Tim Schroeder <timschroeder@vidder.com>
2015-05-30 20:34:17 -0700

    Support upgrade and custom files

--- recipes/idp_installation.rb
+++ recipes/idp_installation.rb
@@ -21,16 +21,28 @@
 
 keystore_password = ShibbolethIdP.get_keystore_password(node)
 
+# Setup Checksum
+
+shibboleth_zip_checksum = node['shibboleth-idp']['checksum']
+if node['shibboleth-idp']['version'] == "2.3.8"
+  shibboleth_zip_checksum ||= "332e5ec32a494ae44c895e2ba21af1ea5ea6665cb66339bef4db1e818cf02e56" #Checksum for version 2.3.8
+end
+
 # Download installer
 
 remote_file_name = "shibboleth-identityprovider-#{node['shibboleth-idp']['version']}-bin.zip"
 
 remote_file "#{Chef::Config['file_cache_path']}/#{remote_file_name}" do
   source "http://shibboleth.net/downloads/identity-provider/#{node['shibboleth-idp']['version']}/#{remote_file_name}"
-  action :create_if_missing
   mode "0744"
   owner "root"
   group "root"
+  if shibboleth_zip_checksum
+    checksum shibboleth_zip_checksum
+  else
+    not_if { File.exists?("/opt/shibboleth-idp/war") }
+  end
+  notifies :run, 'execute[extract_shibboleth_idp_installer]', :immediately
 end
 
 # Install Shibboleth, see https://wiki.shibboleth.net/confluence/display/SHIB2/IdPInstall
@@ -39,7 +51,7 @@ execute "extract_shibboleth_idp_installer" do
   command "jar -xf shibboleth-identityprovider-#{node['shibboleth-idp']['version']}-bin.zip"
   cwd Chef::Config['file_cache_path']
   creates "#{Chef::Config['file_cache_path']}/shibboleth-identityprovider-#{node['shibboleth-idp']['version']}"
-  not_if { File.exists?("/opt/shibboleth-idp/war") }
+  action :nothing
 end
 
 # Template installer properties 
@@ -52,7 +64,7 @@ template "#{Chef::Config['file_cache_path']}/shibboleth-identityprovider-#{node[
   variables(
     :keystore_password => keystore_password
   )
-  not_if { File.exists?("/opt/shibboleth-idp/war") }
+  notifies :run, 'execute[should_install_shibboleth]', :immediately
 end
 
 # Customize the webapp
@@ -63,7 +75,7 @@ template "#{Chef::Config['file_cache_path']}/shibboleth-identityprovider-#{node[
   mode "0644"
   owner "root"
   group "root"
-  not_if { File.exists?("/opt/shibboleth-idp/war") }
+  notifies :run, 'execute[should_install_shibboleth]', :immediately
 end
 
 %w{ error-404.jsp error.jsp login-error.jsp login.css login.jsp }.each do |file|
@@ -73,11 +85,28 @@ end
     mode "0644"
     owner "root"
     group "root"
-    not_if { File.exists?("/opt/shibboleth-idp/war") }
+    notifies :run, 'execute[should_install_shibboleth]', :immediately
   end
 end
 
 # Inject custom files into the webapp
+
+custom_files = node['shibboleth-idp']['custom_webapp_files']
+custom_files ||= []
+
+custom_files.each do |file|
+  cookbook_file "#{Chef::Config['file_cache_path']}/shibboleth-identityprovider-#{node['shibboleth-idp']['version']}/src/main/webapp/#{file}" do
+    source "#{file}"
+    cookbook node['shibboleth-idp']['template_cookbook']
+    mode "0644"
+    owner "root"
+    group "root"
+    notifies :run, 'execute[should_install_shibboleth]', :immediately
+  end
+end
+
+
+# Inject custom templates into the webapp
 
 custom_templates = node['shibboleth-idp']['custom_webapp_templates']
 custom_templates ||= []
@@ -89,20 +118,28 @@ custom_templates.each do |file|
     mode "0644"
     owner "root"
     group "root"
-    not_if { File.exists?("/opt/shibboleth-idp/war") }
+    notifies :run, 'execute[should_install_shibboleth]', :immediately
   end
 end
 
+# Set flag to run installer
+
+execute "should_install_shibboleth" do
+  command "touch /tmp/should_install_shibboleth"
+  action :nothing
+end
+
 # Run the installer 
-# TODO: Figure you how to re-run when one of the above templates change
 
 execute "run_shibboleth_idp_installer" do
   command <<-COMMAND    
+    rm -f /tmp/should_install_shibboleth
     chmod u+x install.sh
     ./install.sh
     chown -R #{node['shibboleth-idp']['owner']}:#{node['shibboleth-idp']['group']} #{node['shibboleth-idp']['idp_home']}
   COMMAND
   cwd "#{Chef::Config['file_cache_path']}/shibboleth-identityprovider-#{node['shibboleth-idp']['version']}"
-  not_if { File.exists?("/opt/shibboleth-idp/war") }
+  action :run
+  only_if 'test -f /tmp/should_install_shibboleth'
   notifies :restart, "service[tomcat]", :delayed
 end
